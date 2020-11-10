9051eb0e4193ff893d11c3f032e388c456717a7d
Sivaskandharuban <65588953+Sivaskandharuban@users.noreply.github.com>
2020-11-10 16:54:23 +0530

    Create router.js
    Post API Implementation

---
+++ router.js
@@ -0,0 +1,47 @@
+var jsforce = require("jsforce");
+// Salesforce OAuth2 client informationvar conn = new jsforce.Connection({  oauth2: {    // you can change loginUrl to connect to sandbox or prerelease env.    loginUrl: "https://login.salesforce.com",    clientId: process.env.Consumer_Key,    clientSecret: process.env.Consumer_Secret,    redirectUri: process.env.Callback_URL,  },});
+//all the routes for our applicationmodule.exports = function (app, db, pgp) {  // =====================================  // HOME PAGE (with login links) ========  // ===================================== app.get("/api/getlead", function (req, res) {    //res.render("index.ejs"); // load the index.ejs file  const results = [];    var query = "SELECT * FROM lead ";    db.query(query, true)      .then(function (data) {        return res.json(data);      })      .catch(function (err) {        console.log("ERROR:", err); // print the error;        return res.status(400).json({ success: false, error: err });      })   .finally(function () {        pgp.end(); // for immediate app exit, closing the connection pool.      });  });
+  
+app.post("/api/createperson", function (req, res) {       
+    const { personid, city } = req.body;   
+    var insertQuery ="INSERT INTO persons (personid, city) VALUES ('" +     personid +     "','" +     city +     "')";
+    db.query(insertQuery, true)     
+    .then(function (data) {       
+        return res.json(data);     })     
+        .catch(function (err) {       
+            console.log("ERROR:", err); 
+        }
+            // print the error;     return res.status(400).json({ success: false, error: err });     })     .finally(function () {       pgp.end(); 
+                // for immediate app exit, closing the connection pool.     }); });
+
+// app.post("/api/updatelead", function (req, res) 
+//     {     
+//         var updateQuery =      "UPDATE lead SET company = 'PashtekUS' WHERE company = 'Pashtek'";
+//     db.query(updateQuery, true)      
+//     .then(function (data) {        
+//         return res.json(data);      
+//     })           
+//     .catch(function (err) {        
+//         console.log("ERROR:", err); 
+//         // print the error;        
+//         return res.status(400).json({ success: false, error: err });      })      .finally(function () {        pgp.end(); // for immediate app exit, closing the connection pool.      });  });    app.post("/api/createlead", function (req, res) {        const { name, company } = req.body;    var insertQuery =      "INSERT INTO lead (name, company) VALUES ('" +      name +      "','" +      company +      "')";
+//     db.query(insertQuery, true)      
+//     .then(function (data) {        
+//         return res.json(data);      
+//     })      
+//     .catch(function (err) {        
+//         console.log("ERROR:", err); 
+//         // print the error;        
+//         return res.status(400).json({ success: false, error: err });
+//     })      
+//     .finally(function () {
+//                 pgp.end(); 
+//                 // for immediate app exit, closing the connection pool.      
+//             });  
+//         });
+
+// app.delete("/api/deletelead", function (req, res) {     
+//     var deleteQuery =      "DELETE FROM lead WHERE company = 'PashtekUS'";
+//     db.query(deleteQuery, true)      .then(function (data) {        return res.json(data);      })           .catch(function (err) {        console.log("ERROR:", err); // print the error;        return res.status(400).json({ success: false, error: err });      })      .finally(function () {        pgp.end(); // for immediate app exit, closing the connection pool.      });  });
+        
+        )});
